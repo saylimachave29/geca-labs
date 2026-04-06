653856fdd28f7637cc7c1e4a890a08ce742596eb
Sayli Machave <saylimachave59@gmail.com>
2026-04-06 16:45:18 +0000

    13 completed

--- 13_puzzle_silent_dept.sql
+++ 13_puzzle_silent_dept.sql
@@ -3,4 +3,13 @@
 
 USE join_lab;
 
--- TODO: Use an outer join or NOT EXISTS pattern; return dept_name only
+USE join_lab;
+
+-- Puzzle B: Finding the "Outsider"
+SELECT 
+    staff.name AS outsider_name, 
+    projects.title AS project_title
+FROM staff
+JOIN project_staff ON staff.staff_id = project_staff.staff_id
+JOIN projects ON project_staff.proj_id = projects.proj_id
+WHERE staff.dept_id != projects.dept_id;
