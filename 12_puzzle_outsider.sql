381d75e3ad6cf3df3dc294f393d03ea91a7716dd
Sayli Machave <saylimachave59@gmail.com>
2026-04-06 16:43:41 +0000

    12 completed

--- 12_puzzle_outsider.sql
+++ 12_puzzle_outsider.sql
@@ -4,4 +4,12 @@
 
 USE join_lab;
 
--- TODO: SELECT name, title (or equivalent) — rows for every such outsider
+
+-- Puzzle B: Finding the "Outsider"
+SELECT 
+    staff.name AS outsider_name, 
+    projects.title AS project_title
+FROM staff
+JOIN project_staff ON staff.staff_id = project_staff.staff_id
+JOIN projects ON project_staff.proj_id = projects.proj_id
+WHERE staff.dept_id != projects.dept_id;
